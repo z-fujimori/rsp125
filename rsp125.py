aeb7ded98742ec544bc7b62673bf743757bc0d77
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-11-08 20:13:41 +0900

    環境を変えて繰り返し学習可

--- rsp125.py
+++ rsp125.py
@@ -17,8 +17,12 @@ class RSP125(gym.Env):
         self.action_space = gym.spaces.Discrete(3) # グー、チョキ、パー
         self.reward_range = 0.0, 5.0 # 報酬の最小値、最大値
         self.observation_space = gym.spaces.MultiDiscrete((4,) * (2 * n_history))
+        # self.hand_percentafe_space_between = 45
+        # self.hand_percentafe_space = [[0, 0, 0] for _ in range(222)]
+        # self.hand_percentafe_space_count = [0, 0] # カウント, 初めの10回か
 
         self.opp = opp or UniformAgent()
+        # self.opp = opp or TitForTat()
         self.n_history = n_history
         self.goal = goal
 
@@ -33,6 +37,9 @@ class RSP125(gym.Env):
 
     def _get_info(self):
         return {'reward_history': self._reward_history[:self.game_count]}
+    
+    def get_hist_info(self):
+        return {'reward_history': self._reward_history[:self.game_count]}
 
     def _get_reward(self, action0, action1):
         result = (4 + action1 - action0) % 3 - 1 # 負け -1, 引き分け 0, 勝ち 1
@@ -46,6 +53,7 @@ class RSP125(gym.Env):
         self.game_count = 0
         self._action_history = np.full((self.n_history + self.goal, 2), 3, dtype=int)
         self._reward_history = np.zeros((self.goal, 2))
+        # self.hand_percentafe_space = [[0, 0, 0] for _ in range(222)]
         return self._get_obs(), self._get_info()
 
     def step(self, action):
@@ -56,6 +64,11 @@ class RSP125(gym.Env):
         self.game_count += 1
         terminated = self.game_count == self.goal
         truncated = False
+
+        # if self.game_count >= 10 :
+        #     self.hand_percentafe_space[(self.game_count-10) // self.hand_percentafe_space_between][]
+        print("-----")
+
         self.render()
         return self._get_obs(), reward, terminated, truncated, self._get_info()
 
@@ -77,7 +90,6 @@ class UniformAgent:
     def get_action(self, obs):
         return self.rng.choice((0, 1, 2), p=(1 / 3, 1 / 3, 1 / 3))
 
-
 class InputAgent:
     def action(self, obs):
         print('履歴')
@@ -88,7 +100,43 @@ class InputAgent:
         while action not in Actions.__members__:
             action = input('R or S or P ? ')
         return Actions[action]
+    
+class NashAgent(UniformAgent):
+    def get_action(self, obs):
+        return self.rng.choice((0, 1, 2), p=(2 / 17, 10 / 17, 5 / 17))
+    
+class TitForTat(NashAgent):
+    def get_hand(self, obs):
+        rng = np.random.default_rng()
+        last_strategy_hand = obs[-1]
+        last_opponent_hand = obs[-2]
+        second_last_opponent_hand = obs[-4]
+        therd_last_opponent_hand = obs[-6]
+        forth_last_opponent_hand = obs[-8]
+    # 繰り返し対策
+        if (last_opponent_hand == second_last_opponent_hand) and (last_opponent_hand == therd_last_opponent_hand) and (last_opponent_hand == forth_last_opponent_hand):
+            if rng.random() >= 1/8:
+                return win_hand(last_opponent_hand)
+        elif (last_opponent_hand == second_last_opponent_hand) and (last_opponent_hand == therd_last_opponent_hand) :
+            if rng.random() >= 1/4:
+                return win_hand(last_opponent_hand)
+        elif (last_opponent_hand == second_last_opponent_hand):
+            if rng.random() >= 1/2:
+                return win_hand(last_opponent_hand)
+        if last_strategy_hand == 2 and last_opponent_hand == 0:
+            return Actions["R"]
+        elif last_strategy_hand == 0 and last_opponent_hand == 2:
+            return Actions["P"]
+        else:
+            return self.np_random.choice(self.hands, p=self.ratio)
 
+def win_hand(hand):
+    if hand == 0:
+        return Actions["P"]
+    elif hand == 1:
+        return Actions["R"]
+    else:
+        return Actions["S"]
 
 def test_run():
     env = RSP125(goal=10, render_mode='human')
@@ -98,7 +146,7 @@ def test_run():
     while not done:
         action = agent.action(obs)
         obs, reward, terminated, truncated, info = env.step(action)
-        env.render()
+        # env.render()
         done = terminated or truncated
     env.close()
 
