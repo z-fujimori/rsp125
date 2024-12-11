c9b228c5b72b5b56f99081aa8562db3dfaa2b936
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-12-11 23:33:31 +0900

    csvに保存　試行開始

--- data_display.py
+++ data_display.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.font_manager as fm
+import csv
 
 def plot_rews(rews1,rews2,id,learning_rate):
   # x軸の値
@@ -28,7 +29,7 @@ def plot_rews(rews1,rews2,id,learning_rate):
   # 表示
   plt.show()
 
-def display_percentage_of_hand(hist_a, hist_b):
+def display_percentage_of_hand(hist_a, hist_b, output_file_name='output.csv'):
   percentage = np.zeros((3, 3), dtype=int) 
   total_pers = []
 
@@ -39,10 +40,25 @@ def display_percentage_of_hand(hist_a, hist_b):
     if i%100 == 99:
       total_pers.append(percentage)
       percentage = np.zeros((3, 3), dtype=int) 
+  
+  # ターミナルに表示
+  # for i, per in enumerate(total_pers):
+  #   print(f'{i+1}')
+  #   print(f'a\\b G | C | P |')
+  #   print(f'G | {per[0][0]:02}| {per[0][1]:02}| {per[0][2]:02}|')
+  #   print(f'C | {per[1][0]:02}| {per[1][1]:02}| {per[1][2]:02}|')
+  #   print(f'P | {per[2][0]:02}| {per[2][1]:02}| {per[2][2]:02}|')
 
-  for i, per in enumerate(total_pers):
-    print(f'{i+1}')
-    print(f'a\\b G | C | P |')
-    print(f'G | {per[0][0]:02}| {per[0][1]:02}| {per[0][2]:02}|')
-    print(f'C | {per[1][0]:02}| {per[1][1]:02}| {per[1][2]:02}|')
-    print(f'P | {per[2][0]:02}| {per[2][1]:02}| {per[2][2]:02}|')
+  # CSV保存部分
+  with open(output_file_name, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    # ヘッダーを書き込む
+    writer.writerow(["Round", "a\\b", "G", "C", "P"])
+    for i, per in enumerate(total_pers):
+      # ラウンド名
+      writer.writerow([f'Round {i+1}'])
+      # データ行を書き込む
+      writer.writerow(["", "G", *per[0]])
+      writer.writerow(["", "C", *per[1]])
+      writer.writerow(["", "P", *per[2]])
+      writer.writerow([])  # 空行を挿入して次のラウンドと区切る
--- opp_buffer/two_dqn.py
+++ opp_buffer/two_dqn.py
@@ -8,6 +8,10 @@ from data_display import plot_rews,display_percentage_of_hand
 
 logger = configure("logs/custom_logs", ["stdout", "csv"])
 
+learn_rate = 0.0005   #  学習りつ DQNのデフォルトは1e-3
+gamma = 0.99    #    割引率   デフォルトは0.99
+learn_count = 1
+
 def main(goal=100):
   act_len_0 = []
   act_len_1 = []
@@ -20,8 +24,9 @@ def main(goal=100):
     learning_starts=0,
     gradient_steps=0,
     verbose=0,
+    learning_rate=learn_rate   #  学習りつ
   )
-  model1 = DQN("MlpPolicy", dummy_env)
+  model1 = DQN("MlpPolicy", dummy_env, learning_rate=learn_rate)   #  学習りつ
   model0.set_env(RSP125(opp=model1, goal=100))
   model1.set_env(RSP125(opp=model0, goal=100)) # 実際は使わないので不要かも
   model0.set_logger(logger)
@@ -32,12 +37,17 @@ def main(goal=100):
   model1.replay_buffer = model0.opp_replay_buffer
   print(model0.replay_buffer.size)
 
-  for i in range(20):
+  for i in range(learn_count):
     rew_0 = 0
     rew_1 = 0
     model0.learn(total_timesteps=1_000, log_interval=100) # log_interval を調整で表示間隔
     reward0 = model0.replay_buffer.rewards.sum()
-    reward1 = model1.replay_buffer.rewards.sum()
+    # reward1 = model1.replay_buffer.rewards.sum()  ここに疑問？
+    reward1 = model0.opp_replay_buffer.rewards.sum()
+
+    print(model0.replay_buffer)
+    print(model0.opp_replay_buffer)
+
     print(f"i: {i}, reward0: {reward0}, reward1: {reward1}")
     model0.logger.record("reward/player_0", reward0)
     model0.logger.record("reward/player_1", reward1)
@@ -65,8 +75,9 @@ def main(goal=100):
   print(len(model0.replay_buffer.rewards))
   model0.logger.record("custom_metric", 42)
   # print(model0.replay_data)
-  display_percentage_of_hand(act_len_0, act_len_1)
-  plot_rews(rew_len_0, rew_len_1, "id", "?")
+  output_file_name = 'two_player_' + str(learn_rate) + "_" + str(learn_count) + "_1.csv"
+  display_percentage_of_hand(act_len_0, act_len_1, output_file_name)
+  plot_rews(rew_len_0, rew_len_1, "id", learn_rate)
   
 
 if __name__ == "__main__":
